3793c89b15f6f9d07a6ee88e535a28cae388b3f0
loneprog <wolfdon73@gmail.com>
2025-12-02 12:51:50 +0100

    Refactor index.js for consistent formatting and improved readability

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
 // Create a new client instance
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: 'creds'
-    })
+  authStrategy: new LocalAuth({
+    dataPath: "creds",
+  }),
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
 });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Client is ready!');
+client.once("ready", () => {
+  console.log("Client is ready!");
 });
 
 // When the client received QR-Code
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-    console.log('QR Code received, scan please!');
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+  console.log("QR Code received, scan please!");
 });
 
 // Start your client
